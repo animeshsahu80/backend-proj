457cf424186ee633b6c1ce39968c3da7c6217fba
Animesh sahu <animesh.sahu@xpo.com>
2024-06-03 02:30:01 +0530

    util code added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-
-// require('dotenv').config()  
+// require('dotenv').config()
 
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app is listning on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("connection failed");
+  });
 
 // const app=express();
 
 // ;
 // (async()=>{
 //     try {
-//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`); 
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 //         app.on("error",(error)=>{
 //             console.log(error);
 //             throw error;
